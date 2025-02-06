5d4d2871ece64f2529c368901f343a793b224228
pawan <kumarpawan9041491@gmail.com>
2025-02-06 18:47:40 +0530

    Initial commit of backend and fronted folders

--- backend/CommonFunction/SendSMS.js
+++ backend/CommonFunction/SendSMS.js
@@ -11,8 +11,8 @@ const sentSMS = async (req, res) => {
   const OTP = otpGenerator();
   if (userdata) {
     const message = `Your Verification code is ${OTP}`;
-    const accountSid = "AC46f28b7579f6bd6932adca5efa177d68";
-    const authToken = "247afd9b62b32653a9d23244d914a18c";
+    const accountSid = process.env.TWILIO_ACCOUNT_SID;
+    const authToken = process.env.TWILIO_AUTH_TOKEN;
     const client = require("twilio")(accountSid, authToken);
     client.messages
       .create({
